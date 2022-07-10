2ac11fa0cb5f3200b940e7d5b87ab6b27854636b
Asmaa Hassan Hamza <asmaahassanhamza1@gmail.com>
2022-07-10 23:26:33 +0200

    change filter design

---
+++ Front/src/Components/Filter/CountryData.js
@@ -0,0 +1,32 @@
+const country=
+[
+{ value:"Cairo" , label:"Cairo"},
+{ value:"Giza" , label:"Giza"},
+{ value:"Alexandria" , label:"Alexandria"},
+{ value:"Dakahlia" , label:"Dakahlia"},
+{ value:"Red Sea" , label:"Red Sea"},
+{ value:"Beheira" , label:"Beheira"},
+{ value:"Fayoum" , label:"Fayoum"},
+{ value:"Ismailia" , label:"Ismailia"},
+{ value:"Menofia" , label:"Menofia"},
+{ value:"Minya" , label:"Minya"},
+{ value:"Qaliubiya" , label:"Qaliubiya"},
+{ value:"New Valley" , label:"New Valley"},
+{ value:"Suez" , label:"Suez"},
+{ value:"Aswan" , label:"Aswan"},
+{ value:"Assiut" , label:"Assiut"},
+{ value:"Beni Suef" , label:"Beni Suef"},
+{ value:"Port Said" , label:"Port Said"},
+{ value:"Damietta" , label:"Damietta"},
+{ value:"Sharkia" , label:"Sharkia"},
+{ value:"South Sinai" , label:"South Sinai"},
+{ value:"Kafr Al sheikh" , label:"Kafr Al sheikh"},
+{ value:"Matrouh" , label:"Matrouh"},
+{ value:"Damietta" , label:"Damietta"},
+{ value:"Luxor" , label:"Luxor"},
+{ value:"Qena" , label:"Qena"},
+{ value:"North Sinai" , label:"North Sinai"},
+{ value:"Sohag" , label:"Sohag"},
+];
+
+export default country;
